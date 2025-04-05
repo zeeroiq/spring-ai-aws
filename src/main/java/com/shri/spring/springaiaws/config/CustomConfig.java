1debbd2de36e8cebf412296a84e7bc25cbdb4a9f
zeeroiq <shrikantvishwakarma@outlook.com>
2025-04-06 00:12:38 +0530

    system prompt configuration. closes #2

--- src/main/java/com/shri/spring/springaiaws/config/CustomConfig.java
+++ src/main/java/com/shri/spring/springaiaws/config/CustomConfig.java
@@ -1,15 +1,22 @@
 package com.shri.spring.springaiaws.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class CustomConfig {
 
+    @Value("classpath:/templates/prompt-template.st")
+    private Resource defaultSystemPrompt;
+
     @Bean
     protected ChatClient chatClient(ChatClient.Builder chatClient) {
-        return chatClient.build();
+        return chatClient
+                .defaultSystem(defaultSystemPrompt)
+                .build();
     }
 
 }
