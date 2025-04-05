c202473454d6e4d3c86c827b9d9865e393e1d0ea
zeeroiq <shrikantvishwakarma@outlook.com>
2025-04-05 23:25:18 +0530

    advisor configuration. closes #1

--- src/main/java/com/shri/spring/springaiaws/controller/AdoptionController.java
+++ src/main/java/com/shri/spring/springaiaws/controller/AdoptionController.java
@@ -2,7 +2,12 @@ package com.shri.spring.springaiaws.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @ResponseBody
@@ -10,13 +15,18 @@ import org.springframework.web.bind.annotation.*;
 public class AdoptionController {
 
     private final ChatClient chatClient;
+    private final Map<String, PromptChatMemoryAdvisor> advisorMap = new ConcurrentHashMap<>();
 
     @GetMapping("/{user}/inquire")
     public String inquire(@PathVariable(name = "user") String user,
                            @RequestParam String question) {
+        var advisor = this.advisorMap.computeIfAbsent(user,
+                _ -> PromptChatMemoryAdvisor.builder(new InMemoryChatMemory()).build());
+
         return this.chatClient
                 .prompt()
                 .user(question)
+                .advisors(advisor)
                 .call()
                 .content();
     }
